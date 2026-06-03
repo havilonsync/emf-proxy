d8ac40aa5abbb29dedb7d33fe9d27fe0985a025b
havilonsync <havilon.sync@gmail.com>
2026-06-03 13:23:11 -0500

    edit api gemini

--- api/proxy.js
+++ api/proxy.js
@@ -96,9 +96,10 @@ export default async function handler(req, res) {
         `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent`,
         {
           method: "POST",
-          headers: { "Content-Type": "application/json",
-                   "x-goog-api-key": process.env.GEMINI_KEY,
-                   },
+          headers: {
+            "Content-Type": "application/json",
+            "x-goog-api-key": process.env.GEMINI_KEY,
+          },
           body: JSON.stringify(geminiBody),
         }
       );
